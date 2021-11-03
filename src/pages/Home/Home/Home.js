2abf41f97cb2b909ae1e04e93dbed7305f2fa1fc
musasazib <musasazib@gmail.com>
2021-11-03 23:49:46 +0600

    Initial commit

---
+++ src/pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Services from '../Services/Services';
+import Navigation from '../../Shared/Navigation/Navigation';
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <Services></Services>
+        </div>
+    );
+};
+
+export default Home;
